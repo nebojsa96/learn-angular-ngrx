a0ed6aa6e04f639c325b8ee9995145f3af727db4
nebojsa96 <nebojsa.sretenovic96@gmail.com>
2023-08-16 16:59:51 +0200

    Setting up firebase and jest

--- angular-arch/src/app/app.module.ts
+++ angular-arch/src/app/app.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFireDatabaseModule } from '@angular/fire/compat/database';
+import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
+import { AngularFireStorageModule } from '@angular/fire/compat/storage';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { environment } from '@src/environments/environment';
 
 @NgModule({
   declarations: [
@@ -10,7 +15,11 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireDatabaseModule,
+    AngularFirestoreModule,
+    AngularFireStorageModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- angular-arch/src/environments/environment.dev.ts
+++ angular-arch/src/environments/environment.dev.ts
@@ -1,4 +1,13 @@
 export const environment = {
     production: false,
-    name: 'dev'
+    name: 'dev',
+    firebase: {
+        apiKey: "AIzaSyCqBV_DQIfXT5fOdujTsHKU6xdW2j7hqS8",
+        authDomain: "learn-angular-ngrx.firebaseapp.com",
+        projectId: "learn-angular-ngrx",
+        storageBucket: "learn-angular-ngrx.appspot.com",
+        messagingSenderId: "634680860369",
+        appId: "1:634680860369:web:790a1baee8b04ae922aff0",
+        measurementId: "G-RCY4HD3D21"
+    }
 }
