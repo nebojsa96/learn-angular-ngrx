d7e1ff67813d5c24601e1fff2486e2b3acbc4e28
nebojsa96 <nebojsa.sretenovic96@gmail.com>
2023-08-20 12:20:57 +0200

    Fixed firebase configuration

--- angular-arch/src/app/app.component.ts
+++ angular-arch/src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
+// import { collectionData } from '@angular/fire/firestore';
+// import { Firestore, collection } from 'firebase/firestore';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'angular-arch';
+
+  // item$: Observable<any[]>;
+  // firestore: Firestore = inject(Firestore);
+
+  constructor(
+    private afs: AngularFirestore
+    ) {
+    // const itemCollection = collection(this.firestore, 'test');
+    // this.item$ = collectionData(itemCollection);
+  }
+
+  ngOnInit(): void {
+    // this.item$.subscribe(data=>{
+      // console.log(data);
+    // })
+    this.afs.collection('test').snapshotChanges().subscribe(data=>{
+      console.log(data);
+    });
+  }
 }
--- angular-arch/src/app/app.module.ts
+++ angular-arch/src/app/app.module.ts
@@ -1,13 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { AngularFireModule } from '@angular/fire/compat';
 import { AngularFireDatabaseModule } from '@angular/fire/compat/database';
-import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
 import { AngularFireStorageModule } from '@angular/fire/compat/storage';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { environment } from '@src/environments/environment';
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
+import { environment } from 'environments/environment';
 
 @NgModule({
   declarations: [
@@ -17,8 +17,8 @@ import { environment } from '@src/environments/environment';
     BrowserModule,
     AppRoutingModule,
     AngularFireModule.initializeApp(environment.firebase),
+    AngularFirestoreModule, // for firestore
     AngularFireDatabaseModule,
-    AngularFirestoreModule,
     AngularFireStorageModule
   ],
   providers: [],
